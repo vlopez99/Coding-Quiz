ca89355d8138f7ff95b4e1deffbf7cfc22c7fc31
Vincent Lopez <115746291+vlopez99@users.noreply.github.com>
2022-11-17 21:43:52 -0500

    initial commit

---
+++ Main/assets/js/scores.js
@@ -0,0 +1,28 @@
+function printHighscores() {
+
+    var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+  
+
+    highscores.sort(function (a, b) {
+      return b.score - a.score;
+    });
+  
+    for (var i = 0; i < highscores.length; i += 1) {
+
+      var liTag = document.createElement('li');
+      liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
+  
+      var olEl = document.getElementById('highscores');
+      olEl.appendChild(liTag);
+    }
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem('highscores');
+    window.location.reload();
+  }
+  
+  document.getElementById('clear').onclick = clearHighscores;
+  
+  printHighscores();
+  
